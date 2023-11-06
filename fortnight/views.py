616b9c67b14edf625c01c1118e9737cb07d5ce39
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-06 12:30:48 +0300

    feat: predict view

--- fortnight/views.py
+++ fortnight/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from fortnight.serializers import PredictSerializer
+from fortnight.utils.logistic_regression_classifier import HeartDiseaseLogisticRegressionCLassifier
 
-# Create your views here.
+model = HeartDiseaseLogisticRegressionCLassifier()
+
+
+@api_view(['POST'])
+def predict(request):
+    """
+    Make a prediction
+    """
+    if request.method == 'POST':
+        serializer = PredictSerializer(data=request.data)
+        if serializer.is_valid():
+
+            X_dict = serializer.save()
+            prediction = model.predict(X_dict)
+
+            return Response(prediction, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
